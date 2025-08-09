c7be67cbd7675a2a411856a763c39c548e9374db
JAGMAGLIGHTS-COM <yourkyrin@gmail.com>
2025-08-09 15:47:13 +0530

    Delete public directory

--- public/widget.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('chat-popup').addEventListener('click', function() {
-  const chatButtons = document.getElementById('chat-buttons');
-  chatButtons.style.display = chatButtons.style.display === 'flex' ? 'none' : 'flex';
-});
-
-// Optional auto-popup after 10s
-setTimeout(() => {
-  document.getElementById('chat-popup').style.display = 'block';
-}, 10000);
